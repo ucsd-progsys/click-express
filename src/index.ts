d505a3a8a35a73caad8a8053ca1b3719d5ca4402
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-07-25 12:00:18 -0700

    tweak tsd

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+/// <reference path='../typings/node/node.d.ts' />
+/// <reference path='../typings/express/express.d.ts' />
+// <reference path='../typings/errorhandler/errorhandler.d.ts' />
+
+import express = require('express');
+// import errorHandler = require("errorhandler");
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+// 404 page
+app.use(function(req, res){
+  res.type('text/plain');
+  res.status(404);
+  res.send("404 - I still havent found what you're looking for");
+  });
+
+// 500 page
+app.use((err: any, req:any, res:any, next:any) => {
+   res.status(err.status || 500);
+   res.type('text/plain');
+   res.send("500 - Yikes! Server error!");
+});
+
+app.listen(app.get('port'), function(){
+  var msg = "Express START: http://localhost:" +
+            app.get('port') +
+            " press Ctrl-C to kill.";
+  console.log(msg);
+});
